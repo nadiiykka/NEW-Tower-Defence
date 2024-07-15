90a75d8eeac6f42cda2d1dae6cb8d66248700130
Nadiia Haidash <nadiia.haidash@vuoksi.fi>
2024-07-15 11:14:17 +0300

    Tower defense #4 в Unity

---
+++ NEW Tower Defence/Assets/C#/Loader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject manager;
+
+    void Awake()
+    {
+        if (Manager.instance == null)
+        {
+            Instantiate(manager);
+        }
+    }
+}
