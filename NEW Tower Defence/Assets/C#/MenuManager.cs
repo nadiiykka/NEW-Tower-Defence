bb9d9fc3154e41b839492b4bc392cdc1d9929658
Nadiia Haidash <nadiia.haidash@vuoksi.fi>
2024-07-18 01:55:47 +0300

    1:53

--- NEW Tower Defence/Assets/C#/MenuManager.cs
+++ NEW Tower Defence/Assets/C#/MenuManager.cs
@@ -5,7 +5,7 @@ public class MenuManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("CutScene");
     }
 
     public void QuitGame()
