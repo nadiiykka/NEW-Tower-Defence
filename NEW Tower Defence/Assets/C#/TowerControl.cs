a2590c5567e1913423f15ac7005d9cbd66eba624
Nadiia Haidash <nadiia.haidash@vuoksi.fi>
2024-07-25 22:10:42 +0300

    Tower defense #12

--- NEW Tower Defence/Assets/C#/TowerControl.cs
+++ NEW Tower Defence/Assets/C#/TowerControl.cs
@@ -8,20 +8,102 @@ public class TowerControl : MonoBehaviour
     float timeBetweenAttacks;
     [SerializeField]
     float attackRadius;
+    [SerializeField]
     Projectile projectile;
     Enemy targetEnemy = null;
     float attackCounter;
+    bool isAttacking = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        attackCounter -= Time.deltaTime;
+
+        if (targetEnemy == null)
+        {
+            Enemy nearestEnemy = GetNearestEnemy();
+
+            if (nearestEnemy != null && UnityEngine.Vector2.Distance(transform.localPosition, nearestEnemy.transform.localPosition) <= attackRadius)
+            {
+                targetEnemy = nearestEnemy;
+            }
+        }
+        else
+        {
+            if (attackCounter <= 0)
+            {
+                isAttacking = true;
+                attackCounter = timeBetweenAttacks;
+            }
+            else
+            {
+                isAttacking = false;
+            }
+            if (UnityEngine.Vector2.Distance(transform.localPosition, targetEnemy.transform.localPosition) > attackRadius)
+            {
+                targetEnemy = null;
+            }
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (isAttacking == true)
+        {
+            Attack();
+        }
+    }
+
+    public void Attack()
+    {
+        isAttacking = false;
+        Projectile newProjectile = Instantiate(projectile) as Projectile;
+        newProjectile.transform.localPosition = transform.localPosition;
+
+        if (targetEnemy == null)
+        {
+            Destroy(newProjectile);
+        }
+        else
+        {
+            StartCoroutine(MoveProjectile(newProjectile));
+        }
+    }
+
+    IEnumerator MoveProjectile(Projectile projectile)
+    {
+        while (GetTargetDistance(targetEnemy) > 0.20f && projectile != null && targetEnemy != null)
+        {
+            var dir = targetEnemy.transform.localPosition - transform.localPosition;
+            var angleDirection = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            projectile.transform.rotation = Quaternion.AngleAxis(angleDirection, UnityEngine.Vector3.forward);
+            projectile.transform.localPosition = UnityEngine.Vector2.MoveTowards(projectile.transform.localPosition, targetEnemy.transform.localPosition, 5f * Time.deltaTime);
+            yield return null;
+        }
+
+        if (projectile != null || targetEnemy == null)
+        {
+            Destroy(projectile);
+        }
+    }
+    private float GetTargetDistance(Enemy thisEnemy)
+    {
+        if (thisEnemy == null)
+        {
+            thisEnemy = GetNearestEnemy();
+            if (thisEnemy == null)
+            {
+                return 0f;
+            }
+        }
+
+        return Mathf.Abs(UnityEngine.Vector2.Distance(transform.localPosition, thisEnemy.transform.localPosition));
     }
 
     private List<Enemy> GetEnemiesInRange()
@@ -30,7 +112,7 @@ public class TowerControl : MonoBehaviour
 
         foreach (Enemy enemy in Manager.Instance.EnemyList)
         {
-            if (UnityEngine.Vector2.Distance(transform.position, enemy.transform.position) <= attackRadius)
+            if (UnityEngine.Vector2.Distance(transform.localPosition, enemy.transform.localPosition) <= attackRadius)
             {
                 enemiesInRange.Add(enemy);
             }
@@ -45,9 +127,9 @@ public class TowerControl : MonoBehaviour
 
         foreach (Enemy enemy in GetEnemiesInRange())
         {
-            if (UnityEngine.Vector2.Distance(transform.position, enemy.transform.position) < smallestDistance)
+            if (UnityEngine.Vector2.Distance(transform.localPosition, enemy.transform.localPosition) < smallestDistance)
             {
-                smallestDistance = UnityEngine.Vector2.Distance(transform.position, enemy.transform.position);
+                smallestDistance = UnityEngine.Vector2.Distance(transform.localPosition, enemy.transform.localPosition);
                 nearestEnemy = enemy;
             }
         }
