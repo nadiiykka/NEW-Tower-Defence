452eb31ce77e721e782219b9a9197def64ab1e30
Nadiia Haidash <nadiia.haidash@vuoksi.fi>
2024-07-25 22:57:18 +0300

    Tower defense #13

--- NEW Tower Defence/Assets/C#/Enemy.cs
+++ NEW Tower Defence/Assets/C#/Enemy.cs
@@ -8,22 +8,34 @@ public class Enemy : MonoBehaviour
     Transform[] wayPoints;
     [SerializeField]
     float navigation;
+    [SerializeField]
+    int health;
 
     int target = 0;
     Transform enemy;
+    Collider2D enemyCollider;
+    Animator anim;
     float navigationTime = 0;
+    bool isDead = false;
+
+    public bool IsDead
+    {
+        get { return isDead; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<Transform>();
+        enemyCollider = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
         Manager.Instance.RegisterEnemy(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (wayPoints != null)
+        if (wayPoints != null && isDead == false)
         {
             navigationTime += Time.deltaTime;
             if(navigationTime > navigation)
@@ -50,5 +62,29 @@ public class Enemy : MonoBehaviour
         {
             Manager.Instance.UnregisterEnemy(this);
         }
+        else if (collision.tag == "Projectile")
+        {
+            Projectile newP = collision.gameObject.GetComponent<Projectile>();
+            EnemyHit(newP.AttackDamage);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void EnemyHit(int hitPoints)
+    {
+        if (health - hitPoints > 0)
+        {
+            health -= hitPoints;
+        }
+        else
+        {
+            anim.SetTrigger("didDie");
+            Die();
+        }
+    }
+    public void Die()
+    {
+        isDead = true;
+        enemyCollider.enabled = false;
     }
 }
